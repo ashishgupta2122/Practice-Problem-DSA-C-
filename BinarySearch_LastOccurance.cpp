f51edcfa3e1f3fd67cd7d7cf948d259eb51f1d5f
Ashish Gupta <105555634+ashishgupta2122@users.noreply.github.com>
2022-06-12 14:52:55 +0530

    Code

--- BinarySearch_LastOccurance.cpp
+++ BinarySearch_LastOccurance.cpp
@@ -1,43 +1,41 @@
 #include<iostream>
 using namespace std;
-int BinarySearch(int arr[],int n,int key)
+int Last_Occurance(int arr[],int n,int key)
 {
     int start = 0;
     int end = n - 1;
     int mid = 0;
     int ans = -1;
-
-    //1st step
-    while(start <= end)
+    
+    //1st Step
+    while (start <= end)
     {
         //2nd Step
         mid = start + (end - start)/2;
         if(arr[mid] == key)
         {
             ans = mid;
-            end = mid - 1;
+            start = mid + 1;
         }
-        //Go to Right Part
-        if(key > arr[mid])
+        else if(key > arr[mid])////Go to Right Part
         {
             start = mid + 1;
         }
-        //Go to Left Part
-        else
+        else ////Go to Left Part
         {
             end = mid - 1;
         }
-        //Update Value
         mid = start + (end - start)/2;
     }
     return ans;
+    
 }
 int main()
 {
-    int arr[5] = {3,9,9,13,27};
-    int key = 9;
-    int index = BinarySearch(arr,5,key);
-    cout<<"Index of First Occurance is "<< index <<endl;
+    int arr[5] = {1,2,3,3,5};
+    int key = 3;
+    int index = Last_Occurance(arr,5,key);
+    cout<<"Index of Last Occurance "<< index <<endl;
 
     return 0;
 }
